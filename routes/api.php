ce839d78ae0554322307f8744c800b7a427c4d50
Sarah Mensah <mansahmensah18@gmail.com>
2024-02-16 10:24:24 +0000

    added web api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GoodController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
+use App\Http\Controllers\StoreTransactionController;
+use App\Http\Controllers\WarehouseTransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +30,43 @@ Route::prefix('categories')->group(function () {
     Route::get('/{id}', [CategoryController::class, 'show']);
     Route::put('/{id}', [CategoryController::class, 'update']);
     Route::delete('/{id}', [CategoryController::class, 'destroy']);
+});
+
+Route::prefix('goods')->group(function () {
+    Route::get('/', [GoodController::class, 'index']);
+    Route::post('/', [GoodController::class, 'store']);
+    Route::get('/{id}', [GoodController::class, 'show']);
+    Route::put('/{id}', [GoodController::class, 'update']);
+    Route::delete('/{id}', [GoodController::class, 'destroy']);
+});
+
+Route::prefix('products')->group(function (){
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::prefix('stores')->group(function () {
+    Route::get('/', [StoreController::class, 'index']);
+    Route::post('/', [StoreController::class, 'store']);
+    Route::get('/{id}', [StoreController::class, 'show']);
+    Route::put('/{id}', [StoreController::class, 'update']);
+    Route::delete('/{id}', [StoreController::class, 'destroy']);
+});
+
+Route::prefix('store_transactions')->group(function () {
+    Route::get('/', [StoreTransactionController::class, 'index']);
+    Route::post('/', [StoreTransactionController::class, 'store']);
+    Route::get('/{id}', [StoreTransactionController::class, 'show']);
+    Route::put('/{id}', [StoreTransactionController::class, 'update']);
+    Route::delete('/{id}', [StoreTransactionController::class, 'destroy']);
+});
+
+Route::prefix('warehouse_transactions')->group(function () {
+    Route::get('/', [WarehouseTransactionController::class, 'index']);
+    Route::post('/', [WarehouseTransactionController::class, 'store']);
+    Route::get('/{id}', [WarehouseTransactionController::class, 'show']);
+    Route::put('/{id}', [WarehouseTransactionController::class, 'update']);
+    Route::delete('/{id}', [WarehouseTransactionController::class, 'destroy']);
 });
